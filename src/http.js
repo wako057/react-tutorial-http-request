758f05d680b45a15b78e84eab0ede28bf2479847
Gregory Gerard <gregory@flatchr.io>
2024-01-22 23:33:36 +0100

    feat: sample put

--- src/http.js
+++ src/http.js
@@ -10,3 +10,22 @@ export async function fetchAvailablePlaces() {
 
   return resData.places;
 };
+
+export async function updateUserPlaces(places) {
+  const response = await fetch('http://localhost:3000/user-places',
+    {
+      method: 'PUT',
+      body: JSON.stringify({places}),
+      headers: {
+        'Content-type': 'application/json'
+      }
+    });
+
+  const resData = await response.json();
+
+  if (!response.ok) {
+    throw new Error('Failed to update user data');
+  }
+
+  return resData.message;
+}
