e01ff95a0416304177844cabd5add37dfb438320
Gregory Gerard <gregory@flatchr.io>
2024-01-22 23:53:57 +0100

    feat: handle error with state

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,13 @@ import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
 import AvailablePlaces from './components/AvailablePlaces.jsx';
 import {updateUserPlaces} from "./http";
+import Error from "./components/Error";
 
 function App() {
   const selectedPlace = useRef();
 
   const [userPlaces, setUserPlaces] = useState([]);
-
+  const [errorUpdatetingPlace, setErrorUpdatingPlaces] = useState();
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
   function handleStartRemovePlace(place) {
@@ -38,7 +39,10 @@ function App() {
     try {
       await updateUserPlaces([ selectedPlace, ...userPlaces]);
     } catch (error) {
-      // ...
+      setUserPlaces(userPlaces);
+      setErrorUpdatingPlaces({
+        message: error.message || 'Failed to update places'
+      })
     }
   }
 
@@ -50,8 +54,20 @@ function App() {
     setModalIsOpen(false);
   }, []);
 
+  function handleError() {
+    setErrorUpdatingPlaces(null);
+  }
+
   return (
     <>
+      <Modal open={errorUpdatetingPlace}>
+        {errorUpdatetingPlace && (<Error
+          onClose={handleError}
+          title="An error occured"
+          message={errorUpdatetingPlace.message}
+          onConfirm={handleError}
+        />)}
+      </Modal>
       <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
