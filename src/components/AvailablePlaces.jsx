1b5e9e0908774af629b3f352ccbe517db2bb0ea4
Gregory Gerard <gregory@flatchr.io>
2024-01-22 09:37:11 +0100

    feat: handle loading part

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -4,19 +4,26 @@ import {useState, useEffect} from "react";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [ availablePlaces, setAvailablePlaces ] = useState([]);
+  const [ isFetching, setIsFetching ] = useState(false);
 
   useEffect(() => {
     async function fetchPlaces() {
+      setIsFetching(true);
       const response = await fetch('http://localhost:3000/places');
       const resData = await response.json();
       setAvailablePlaces(resData.places);
+      setIsFetching(false);
     }
+    fetchPlaces();
   }, []);
+
 
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching places data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
